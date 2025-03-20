0f88a5e24aa0863469068e9bf57c3542742ad500
Filipe Lima <flima@f0r1s11.codam.nl>
2025-03-20 21:54:57 +0100

    workinh on merge_quotes

--- srcs/parsing/merge_tokens.c
+++ srcs/parsing/merge_tokens.c
@@ -6,7 +6,7 @@
 /*   By: flima <flima@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 19:45:28 by flima             #+#    #+#             */
-/*   Updated: 2025/03/20 19:51:43 by flima            ###   ########.fr       */
+/*   Updated: 2025/03/20 21:54:22 by flima            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,11 @@
 // merge_adjacent_tokens like enviroment variables $
 // and double quotes and single. 
 // 
+
+t_pars_err	merge_quotes(t_main_data *data)
+{
+	// usar ft_strjoin
+}
 t_pars_err	merge_adjacent_tokens(t_main_data *data)
 {
 	
