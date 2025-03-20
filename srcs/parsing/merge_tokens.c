2aa6e808ebe603236e5ba30a6b09e863efbecc95
Filipe Lima <flima@f0r1s11.codam.nl>
2025-03-20 21:39:56 +0100

    added merge-tokens.c

---
+++ srcs/parsing/merge_tokens.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   merge_tokens.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flima <flima@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/20 19:45:28 by flima             #+#    #+#             */
+/*   Updated: 2025/03/20 19:51:43 by flima            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "tokenization.h"
+
+// merge_adjacent_tokens like enviroment variables $
+// and double quotes and single. 
+// 
+t_pars_err	merge_adjacent_tokens(t_main_data *data)
+{
+	
+}
