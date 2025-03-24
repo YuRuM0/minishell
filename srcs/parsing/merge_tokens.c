53072cb107ae335c789594ee0a3b7fb0f9d984e0
Filipe Lima <flima@f0r1s4.codam.nl>
2025-03-24 16:07:45 +0100

    added function merge_env_var to merge the tokens after $ if needed

--- srcs/parsing/merge_tokens.c
+++ srcs/parsing/merge_tokens.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   merge_tokens.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: filipe <filipe@student.42.fr>              +#+  +:+       +#+        */
+/*   By: flima <flima@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 19:45:28 by flima             #+#    #+#             */
-/*   Updated: 2025/03/23 23:08:38 by filipe           ###   ########.fr       */
+/*   Updated: 2025/03/24 16:04:23 by flima            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 
 static void remove_next_token(t_token *current)
 {
-	t_token	*tmp_token;
+	t_token *tmp_token;
 
 	tmp_token = current->next;
 	current->next = current->next->next;
@@ -29,10 +29,10 @@ static void remove_next_token(t_token *current)
 static t_pars_err merge_quotes(t_token *current)
 {
 	char *tmp_str;
-	
-	while (current->next && (current->next->type == D_QUOTE || \
-			current->next->type == S_QUOTE || \
-			current->next->type == WORD))
+
+	while (current->next && (current->next->type == D_QUOTE ||
+							 current->next->type == S_QUOTE ||
+							 current->next->type == WORD))
 	{
 		tmp_str = current->value;
 		current->value = ft_strjoin(tmp_str, current->next->value);
@@ -44,6 +44,27 @@ static t_pars_err merge_quotes(t_token *current)
 	return (SUCCESS);
 }
 
+static t_pars_err merge_env_var(t_token *current)
+{
+	char *tmp_str;
+	
+	if (!current->next || (ft_strlen(current->value) == 1 \
+			&& current->next && \
+			(current->next->type == TAB_CHAR || \
+			current->next->type == NEW_LINE || \
+			current->next->type == SPACE_CHAR)))
+		current->type = WORD;
+	else if (current->next && current->next->type == WORD)
+	{
+		tmp_str = current->value;
+		current->value = ft_strjoin(tmp_str, current->next->value);
+		if (current == NULL && errno == ENOMEM)
+			return (ERROR_MEM_ALLOC);
+		free(tmp_str);
+		remove_next_token(current);
+	}
+	return (SUCCESS);
+}
 t_pars_err merge_adjacent_tokens(t_main_data *data)
 {
 	t_token *current;
@@ -51,12 +72,18 @@ t_pars_err merge_adjacent_tokens(t_main_data *data)
 	current = data->tokens;
 	while (current != NULL)
 	{
-		if (current->type == D_QUOTE || \
-			current->type == S_QUOTE || \
+		if (current->type == D_QUOTE ||
+			current->type == S_QUOTE ||
 			current->type == WORD)
+		{
 			if (merge_quotes(current) != SUCCESS)
 				return (ERROR_MEM_ALLOC);
-		// else if(current->type == VARIABLE)
+		}
+		else if (current->type == VARIABLE)
+		{
+			if (merge_env_var(current) != SUCCESS)
+				return (ERROR_MEM_ALLOC);
+		}
 		current = current->next;
 	}
 	return (SUCCESS);
